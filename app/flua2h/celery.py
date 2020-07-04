f929310381019d6f39b5c090d1d7041badb063be
Shan <stwm2@student.london.ac.uk>
2020-07-04 22:59:12 +0800

    Delete old project files from master

--- app/flua2h/celery.py
+++
@@ -1,4 +0,0 @@
-from celery import Celery
-
-app = Celery('flua2h',backend='rpc://',broker='pyamqp://rabbitmq:rabbitmq@viva-rabbitmq//',include=['flua2h.tasks'])
-#app = Celery('flua2h',backend='rpc://',broker='pyamqp://rabbitmq:rabbitmq@viva-rabbitmq//',include=['flua2h.tasks','common.jobmanagement','common.sequence','common.hunana'])
