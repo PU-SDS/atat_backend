44d616ee174063ac162d9974f17ce01b9bd38e0c
Shan <stwm2@student.london.ac.uk>
2020-07-05 19:00:36 +0800

    Implemented trimming using TRIMAL, changed to using job id for class initiation. TODO: Add comments

--- app/preprocess/align.py
+++ app/preprocess/align.py
@@ -1,5 +1,6 @@
 import subprocess
 from io import StringIO
+from os import path
 from Bio import AlignIO
 from Bio.Align import MultipleSeqAlignment
 from Bio.Align.Applications import MafftCommandline
@@ -21,59 +22,73 @@ class Alignment(object):
         Contains all the logic related to MSA including trimming, splitting and alignment itself.
 
         Functions:
-            align(object, jobpath: str)
-            trim(object, file: str) -> str
+            align(object)
+            trim(object)
 
         Constants:
             ALIGNED_MSA_OUTPUT_FILENAME: The filename used for the MAFFT aligned
             sequence file.
 
+            TRIMMED_MSA_OUTPUT_FILENAME: The filename used for the TRIMAI trimmed
+            sequence file.
+
+            JOB_LOCAL_PATH: The local path to the job. This is built automatically
+            when the class is instantiated using the job id and the common job path
+            defined in the configuration file.
+
         Example:
             >>> from app.preprocess import Alignment
-            >>> new_alignment = Alignment(jobpath)
+            >>> new_alignment = Alignment('id_5165432', 0.05)
             >>> new_alignment.align()
             >>> new_alignment.trim()
     """
 
     ALIGNED_MSA_OUTPUT_FILENAME = 'aligned.fasta'
+    TRIMMED_MSA_OUTPUT_FILENAME = 'aligned_trimmed.fasta'
+    JOB_LOCAL_PATH = ''
 
-    def __init__(self, jobpath: str, msa=None):
-        self.jobpath = jobpath
-        self.msa = msa
-
-    def align(self) -> str:
+    def __init__(self, job_id: str, trimal_gt: float = 0.05):
         """
-            Multiple Sequence Alignment Method using MAFFT
-
-            This method carries out MSA alignment using MAFFT.
+            Args:
+                job_id (str): The current job ID.
+                trimal_gt (float): The gap threshold for TRIMAL.
         """
+        self.job_id = job_id
+        self.trimal_gt = trimal_gt
+        self.JOB_LOCAL_PATH = path.join(app.config['JOBS_FOLDER'], self.job_id)
+
+    def align(self):
+        """
+            Multiple Sequence Alignment method using MAFFT
+
+            Carries out MSA alignment using MAFFT.
+        """
+
         mafft_cline = MafftCommandline(app.config['MAFFT'],
-                                       input=self.jobpath + f'/{PreProcessor.OUTPUT_FILENAME}')
+                                       input=path.join(self.JOB_LOCAL_PATH, PreProcessor.OUTPUT_FILENAME))
 
         try:
-            stdout, stderr = mafft_cline()
-            self.msa = stdout
+            std_out, std_err = mafft_cline()
         except ApplicationError as error:
             raise AlignmentUnknownException(f'An unknown error occurred during the sequence '
-                                            f'alignment phase using MAFFT: {error}')
+                                            f'alignment phase using MAFFT: {std_err}')
 
-        alignment = AlignIO.read(StringIO(self.msa), 'fasta')
-        AlignIO.write(alignment, self.jobpath + f'/{self.ALIGNED_MSA_OUTPUT_FILENAME}')
+        alignment = AlignIO.read(StringIO(std_out), 'fasta')
+        AlignIO.write(alignment, path.join(self.JOB_LOCAL_PATH, self.ALIGNED_MSA_OUTPUT_FILENAME))
 
-    def trim(self, file: str):
+    def trim(self):
         """
             Alignment trimming method using TRIMAL
 
-            This method trims the MSA using the user provided gap penalty (default = 0.05).
-
-            Parameters:
-                file (str): The path to the aligned sequences file.
-
-            Returns:
-                A file path to the aligned and trimmed FASTA file.
-
+            Trims the MSA using the user provided gap penalty (default = 0.05).
         """
-        if isinstance(self.msa, MultipleSeqAlignment):
-            trimal = subprocess.Popen(
-                [app.config['TRIMAL'], '']
-            )
+        trimal_args = ['-in', path.join(self.jobpath, self.ALIGNED_MSA_OUTPUT_FILENAME),
+                       '-out', path.join(self.jobpath, self.TRIMMED_MSA_OUTPUT_FILENAME), '-gt', self.trimal_gt]
+
+        trimal = subprocess.Popen([app.config['TRIMAL']] + trimal_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        std_out, std_err = trimal.communicate()
+        return_code = trimal.returncode
+
+        if return_code:
+            raise AlignmentUnknownException(f'An unknown error occurred during the sequence '
+                                            f'trimming phase using TRIMAL: {std_err}')
