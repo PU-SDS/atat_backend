e69743fa0b251dc8d1754756cbd7124eedc66944
Shan <stwm2@student.london.ac.uk>
2020-07-14 14:09:37 +0800

    Added more comments

--- app/hunana/window.py
+++ app/hunana/window.py
@@ -12,10 +12,17 @@ class SlidingWindow(object):
             >>> from app.hunana import SlidingWindow
             >>> window = SlidingWindow(iter[SeqRecord], 9)
             >>> kmers = window.kmers()
-
+            >>> kmer1 = list(kmers[0]) # First k-mer of all sequences
+            >>> kmer2 = list(kmers[1]) # Second k-mer of all sequences
     """
 
     def __init__(self, seqs, kmer_len=9):
+        """
+            Args:
+                seqs: An iterable of type SeqRecord.
+                kmer_len: The length of the sliding window.
+        """
+
         self.seqs = seqs
         self.kmer_len = kmer_len
 
@@ -24,12 +31,12 @@ class SlidingWindow(object):
             Extracts k-mers from all the sequences provided.
 
             Returns:
-                Zip: A Zip object containing all k-mers
+                Zip: A Zip object containing all k-mers.
         """
 
         seqs = map(lambda s: str(s.seq), self.seqs)
-        window_seqs = map(lambda s: self._window(s, self.kmer_len), seqs)
-        return zip(*window_seqs)
+        kmers_seqs = map(lambda s: self._window(s, self.kmer_len), seqs)
+        return zip(*kmers_seqs)
 
     @classmethod
     def _window(cls, seq, kmer_len):
