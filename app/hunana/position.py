891ab588dd4f8c2126e92d0bceaffa79373deecf
Shan <stwm2@student.london.ac.uk>
2020-08-04 17:40:03 +0800

    Hunana completed. TODO: Cleanup, documentation and comments.
    Pretty sure the position module can be drastically improved. Look into that too.

--- app/hunana/position.py
+++ app/hunana/position.py
@@ -3,3 +3,23 @@ class Position(object):
     ENTROPY = None
     VARIANTS = None
     VARIANTS_FLATTENED = None
+
+    @property
+    def get_variant_count(self):
+        return len(self.VARIANTS)
+
+    @property
+    def get_support_count(self):
+        return len(self.VARIANTS_FLATTENED)
+
+class Variant(object):
+    SEQUENCE = None
+    COUNT = None
+    CONSERVATION = None
+
+    def __init__(self, sequence, count, conservation):
+        self.SEQUENCE = sequence
+        self.COUNT = count
+        self.CONSERVATION = conservation
+
+
--- app/hunana/window.py
+++ app/hunana/window.py
@@ -1,8 +1,7 @@
 from collections import Counter
 from itertools import islice
-import random
 
-from app.hunana.position import Position
+from app.hunana.position import Position, Variant
 
 
 class SlidingWindow(object):
@@ -66,10 +65,6 @@ class SlidingWindow(object):
             else:
                 yield 'illegal-char'
 
-    def test(self, nonomers):
-        a = list(map(lambda s: Counter(s), nonomers))
-        return enumerate(map(lambda s: Counter(s), nonomers))
-
     @classmethod
     def _variant_counter(cls, kmers):
         """
@@ -84,27 +79,35 @@ class SlidingWindow(object):
             del counter['illegal-char']
             yield counter
 
-    @classmethod
-    def _create_position_objects(cls, counters):
+    def _create_position_objects(self, counters):
         """
             Create kmer position objects with entropy, position and variant properties
 
             Returns:
                 A Generator object for Position objects for each kmer position
         """
-        print('run once')
+
         for position, counter in enumerate(counters):
             position_object = Position()
-
             position_object.POSITION = position
-            position_object.VARIANTS = counter
+            position_object.VARIANTS = self._create_variant_objects(counter)
             position_object.VARIANTS_FLATTENED = counter.elements()
 
             yield position_object
+
+    def _create_variant_objects(self, variant_counter):
+        variant_count = sum(variant_counter.values())
+        variant_objects = map(lambda x: Variant(x[0], x[1], self._calc_conservation(x[1], variant_count)),
+                              variant_counter.items())
+        return list(variant_objects)
+
+    @classmethod
+    def _calc_conservation(cls, variant_hits, total_hits):
+        conservation = (float(variant_hits) * 100) / float(total_hits)
+        return conservation
 
     def run(self):
         kmers = self._kmers()
         variant_counters = self._variant_counter(kmers)
         kmer_position_objects = self._create_position_objects(variant_counters)
         return kmer_position_objects
-
