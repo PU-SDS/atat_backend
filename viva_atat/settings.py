325f29e3289e54de7a3dfdc0272dcc1ef392a1b1
Shan Tharanga <63629580+ShanWeera@users.noreply.github.com>
2021-09-29 21:00:09 +0800

    Added mongo port

--- viva_atat/settings.py
+++ viva_atat/settings.py
@@ -8,6 +8,7 @@ class ResourceSettings(BaseSettings):
     """
 
     mongo_host: str
+    mongo_port: int
     mongo_root_username: str = None  # Only needed in DEV to set up the database so we default to None in Prod
     mongo_root_password: str = None  # Only needed in DEV to set up the database so we default to None in Prod
 
@@ -32,6 +33,7 @@ class ResourceSettings(BaseSettings):
         # Environment variables will always take priority over values loaded from a dotenv file.
         fields = {
             "mongo_host": {"env": ["MONGO_ATAT_HOST"]},
+            "mongo_port": {"env": ["MONGO_ATAT_PORT"]},
             "mongo_root_username": {"env": ["MONGO_INITDB_ROOT_USERNAME"]},
             "mongo_root_password": {"env": ["MONGO_INITDB_ROOT_PASSWORD"]},
             "mongo_atat_username": {"env": ["MONGODB_ATAT_USERNAME"]},
