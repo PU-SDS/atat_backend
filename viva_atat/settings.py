b136759592afe5d4bc2ef8d616ba8650f25fd32b
Shan Tharanga <63629580+ShanWeera@users.noreply.github.com>
2021-10-01 01:18:21 +0800

    some refactoring

--- viva_atat/settings.py
+++ viva_atat/settings.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from pydantic import BaseSettings
 
 
@@ -7,8 +9,11 @@ class ResourceSettings(BaseSettings):
     For more info: https://pydantic-docs.helpmanual.io/usage/settings/
     """
 
+    env_state: Literal["prod", "dev"] = "dev"  # Change this when going production
+
     mongo_host: str
-    mongo_port: int
+    mongo_port: int = 27017
+    mongo_replicaset_name: str = "rs0"
     mongo_root_username: str = None  # Only needed in DEV to set up the database so we default to None in Prod
     mongo_root_password: str = None  # Only needed in DEV to set up the database so we default to None in Prod
 
@@ -34,6 +39,7 @@ class ResourceSettings(BaseSettings):
         fields = {
             "mongo_host": {"env": ["MONGO_ATAT_HOST"]},
             "mongo_port": {"env": ["MONGO_ATAT_PORT"]},
+            "mongo_replicaset_name": {"env": ["MONGO_REPLICASET_NAME"]},
             "mongo_root_username": {"env": ["MONGO_INITDB_ROOT_USERNAME"]},
             "mongo_root_password": {"env": ["MONGO_INITDB_ROOT_PASSWORD"]},
             "mongo_atat_username": {"env": ["MONGODB_ATAT_USERNAME"]},
